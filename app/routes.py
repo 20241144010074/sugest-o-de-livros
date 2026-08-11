3b87fb3a47495e6f957b55c080c9cd9695a5be9d
Noemy <pamela.messias@escolar.ifrn.edu.br>
2026-08-11 10:27:26 -0300

    feat: implementa cadastro e listagem de livros

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
+from . import db
+from .models import Livro
 
 bp = Blueprint("main", __name__)
 
 
 @bp.route("/")
 def index():
-    return "Sistema de Sugestão de Livros"
+    livros = Livro.query.all()
+    return render_template("index.html", livros=livros)
+
+
+@bp.route("/livros", methods=["POST"])
+def criar_livro():
+    livro = Livro(
+        titulo=request.form["titulo"],
+        autor=request.form["autor"],
+        genero=request.form["genero"],
+        descricao=request.form.get("descricao")
+    )
+
+    db.session.add(livro)
+    db.session.commit()
+
+    return redirect(url_for("main.index"))
