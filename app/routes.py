fc2b98fdefdc019fd9f590b7421b8401add9595d
Noemy <pamela.messias@escolar.ifrn.edu.br>
2026-08-11 10:35:21 -0300

    feat: implementa atualizacao de livros

--- app/routes.py
+++ app/routes.py
@@ -24,3 +24,20 @@ def criar_livro():
     db.session.commit()
 
     return redirect(url_for("main.index"))
+
+
+@bp.route("/livros/<int:id>/editar", methods=["GET", "POST"])
+def editar_livro(id):
+    livro = db.get_or_404(Livro, id)
+
+    if request.method == "POST":
+        livro.titulo = request.form["titulo"]
+        livro.autor = request.form["autor"]
+        livro.genero = request.form["genero"]
+        livro.descricao = request.form.get("descricao")
+
+        db.session.commit()
+
+        return redirect(url_for("main.index"))
+
+    return render_template("editar.html", livro=livro)
